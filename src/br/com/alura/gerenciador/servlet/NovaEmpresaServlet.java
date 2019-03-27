626dfe9bda2c527da7df501f8123d90fb6ee5418
Marcio Jose Lima de Sousa <sotreze@yahoo.com.br>
2019-03-27 14:51:58 -0300

    CAMP - JSTL e Expression Language

--- src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -1,6 +1,9 @@
 package br.com.alura.gerenciador.servlet;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -20,8 +23,19 @@ public class NovaEmpresaServlet extends HttpServlet {
 		System.out.println("Cadastrando uma nova empresa");
 		
 		String nomeEmpresa = request.getParameter("nome");
+		String paraDataEmpresa = request.getParameter("data");
+		
+		Date dataAbertura = null;
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy"); 
+			dataAbertura = sdf.parse(paraDataEmpresa);
+		} catch (ParseException e) {
+			throw new ServletException(e);
+		}
+		
 		Empresa empresa = new Empresa();
 		empresa.setNome(nomeEmpresa);
+		empresa.setDataAbertura(dataAbertura);
 		
 		
 		Banco banco = new Banco();
