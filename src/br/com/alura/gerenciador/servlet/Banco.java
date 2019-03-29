097dbf976965c7fff012f6010beedd8ba09e79f2
Marcio Jose Lima de Sousa <sotreze@yahoo.com.br>
2019-03-29 20:56:39 -0300

    CAMP - Completando o CRUD

--- src/br/com/alura/gerenciador/servlet/Banco.java
+++ src/br/com/alura/gerenciador/servlet/Banco.java
@@ -42,4 +42,13 @@ public class Banco {
 		return Banco.lista;
 	}
 
+	public Empresa buscaEmpresaPeloId(Integer id) {
+		for (Empresa empresa : lista) {
+			if(empresa.getId() == id) {
+				return empresa;
+			}
+		}	
+		return null;
+	}
+
 }
